40ed49d7773f7c2f99bac63b108547159f3da48e
RF80 <RF80>
2017-12-05 21:45:11 +0300

    Перед уборкой лишнего

--- index.php
+++ index.php
@@ -9,8 +9,53 @@
 </head>
 <body>
 
-	<?php	get_header();
-	get_footer();
+	<?php	get_header();?>
+
+<div class="container">
+	<div class="row">
+		<div id="primary" class="col-md-12 content-area">
+			<main id="main" class="site-main">
+
+
+				<div class="article-grid-container">
+					<?php if ( have_posts() ) : ?>
+
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+
+							<?php
+
+							$post_display_option = get_theme_mod('post_display_option','post-excerpt');
+
+							if($post_display_option == 'post-excerpt'){
+								get_template_part( 'template-parts/content','page');
+							}
+							else{
+								get_template_part( 'template-parts/content','page');
+							}
+
+							?>
+
+						<?php endwhile; ?>
+
+						
+
+					<?php else : ?>
+
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+					<?php endif; ?>
+				</div>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!--row-->      
+
+</div><!--.container-->
+
+
+
+	<?php	get_footer();
 	?>	
 </body>
 </html>
---
+++ template-parts/content-page.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * Please browse readme.txt for credits and forking information
+ * @package noteblog
+ */
+function noteblog_featured_image_disaplay(){
+	if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() ) {  // check if the post has a Post Thumbnail assigned to it. 
+		echo '<div class="featured-image">';
+		the_post_thumbnail('noteblog-full-width');
+		echo '</div>';
+	} 
+}
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-content'); ?>>
+
+	<?php noteblog_featured_image_disaplay(); ?>
+
+	<header class="entry-header">
+		<span class="screen-reader-text"><?php the_title();?></span>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta"></div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'noteblog' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
+
