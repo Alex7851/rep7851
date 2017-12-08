21ee7b572267360552922e3eb0ec0ffc1d670785
RF80 <RF80>
2017-12-08 08:02:44 +0300

    addition file

--- mainIndex.php
+++ mainIndex.php
@@ -4,19 +4,13 @@
 
 			<?php
 			$post_display_option = get_theme_mod('post_display_option','post-excerpt');
-
-			if($post_display_option == 'post-excerpt'){
 				get_template_part( 'content','page');
-			}
-			else{
-				get_template_part( 'content','page');
-			}
 			?>
 
 		<?php endwhile; ?>
 
 	<?php else : ?>
 
-		<?php get_template_part( 'template-parts/content', 'none' ); ?>
+		<?php get_template_part( 'content', 'none' ); ?>
 
 	<?php endif; ?>
